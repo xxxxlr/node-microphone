55570dc0f462e9c9e4996887dc94a50ed66c90d1
xxxxlr <xxxxlr@gmail.com>
2018-05-24 13:55:18 +0800

    Added rec

--- index.js
+++ index.js
@@ -1,24 +1,63 @@
 var isMacOrWin = require('os').type() == 'Darwin' || require('os').type().indexOf('Windows') > -1;
 var spawn = require('child_process').spawn
 var PassThrough = require('stream').PassThrough;
-var lame = require('lame');
+// var lame = require('lame');
 
 var ps = null;
 
 var audio = new PassThrough;
 var info = new PassThrough;
 
-var start = function(options) {
+var start = function (options) {
     options = options || {};
-    let device = options['device'] || 'default'
-    
-    if(ps == null) {
-        ps = isMacOrWin
-        ? spawn('sox', ['-d', '-t', 'dat', '-p'])
-        : spawn('arecord', ['-D', device, '-f', 'dat']);
+    options.device = options['device'] || 'default'
+    options.recordProgram = options.recordProgram || 'rec'
+    options.sampleRate = options.sampleRate || 16000
+    options.cmdOptions = options.cmdOptions || {}
+    let ps = null
+    switch (options.recordProgram) {
+        case 'rec':
+            // let cmdOptions = { encoding: 'binary' }
+            if (options.device) {
+                options.cmdOptions.env = Object.assign({}, process.env, { AUDIODEV: options.device })
+            }
+            options.cmdArgs = options.cmdArgs || [
+                '-q',                     // show no progress
+                '-r', options.sampleRate, // sample rate
+                '-c', '1',                // channels
+                '-e', 'signed-integer',   // sample encoding
+                '-b', '16',               // precision (bits)
+                '-t', 'wav',              // audio type
+                '-',                      // pipe
+                // end on silence
+                // 'silence', '1', '0.1', options.thresholdStart || options.threshold + '%',
+                // '1', options.silence, options.thresholdEnd || options.threshold + '%'
+            ]
+            ps = spawn(
+                options.recordProgram,
+                options.cmdArgs,
+                options.cmdOptions)
+            break;
+        case 'sox':
+            options.cmdArgs = options.cmdArgs || ['-d', '-t', 'dat', '-p']
+            break;
 
-        if(options.mp3output === true) {
-            var encoder = new lame.Encoder( {
+        case 'arecord':
+            options.cmdArgs = options.cmdArgs || ['-D', options.device, '-r', 16000, '-f', 'S16_LE']
+            // ps = spawn('arecord', ['-D', options.device, '-r', 16000]);
+            break;
+        default:
+            console.error('Only support the following programs: rec, sox, arecord')
+            break;
+
+            ps = spawn(options.recordProgram,
+                options.cmdArgs,
+                options.cmdOptions);
+            console.log(`raw command line: ${options.recordProgram} ${options.cmdArgs.join(' ')}`)
+    }
+    if (ps !== null) {
+        if (options.mp3output === true) {
+            var encoder = new lame.Encoder({
                 channels: 2,
                 bitDepth: 16,
                 sampleRate: 44100
@@ -34,10 +73,11 @@ var start = function(options) {
 
         }
     }
+    // return ps
 };
 
-var stop = function() {
-    if(ps) {
+var stop = function () {
+    if (ps) {
         ps.kill();
         ps = null;
     }
